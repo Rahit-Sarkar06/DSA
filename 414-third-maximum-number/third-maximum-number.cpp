db006e6bca0ad8f29adbc49f66ba98f92ff0aec5
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-05-19 19:57:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (49.58%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-      int n=nums.size();
-      int max=nums[n-1];
-      int i=n-2;
-      while(i>=0 && nums[i]==max){
-        i--;
-      }
-      if(i<0) return max;
-      int max2=nums[i];
-      
-      int j=i-1;
-      while(j>=0 && nums[j]==max2){
-        j--;
-      }
-      if(j<0){
-        return max;
-      }else{
-        return nums[j];
-      }
+            int n = nums.size();
+        
+        long long first = LLONG_MIN;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > first) {
+                first = nums[i];
+            }
+        }
+        
+        long long second = LLONG_MIN;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > second && nums[i] != first) {
+                second = nums[i];
+            }
+        }
+        
+        long long third = LLONG_MIN;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > third && nums[i] != first && nums[i] != second) {
+                third = nums[i];
+            }
+        }
+        
+        if (third == LLONG_MIN) {
+            return first;
+        }
+        
+        return third;
      
 
     }
