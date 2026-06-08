87639eeaec7e6edf4804ff20e10cb92bf3d7a2de
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-06-08 23:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (94.93%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -4,10 +4,10 @@ public:
         int num1=0;
         int num2=0;
         for(int i=1;i<=n;i++){
-            if(i%m==0){
-                num2+=i;
+            if(i%m!=0){
+                num1+=i;
             }else{
-            num1+=i;
+            num2+=i;
             }
         }
         return num1-num2;
