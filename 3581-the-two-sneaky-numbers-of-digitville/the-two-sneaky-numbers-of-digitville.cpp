47af0b2d98afd56bb311fa70333677d033eff802
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-06-09 19:11:25 +0530

    Time: 4 ms (23.66%) | Memory: 26.4 MB (22.13%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        vector<int>n;
+        int p=nums.size();
+        map<int,int> mpp;
+        for(int i=0;i<p;i++){
+            mpp[nums[i]]++;
+        }
+            for( auto it : mpp ){
+            if(it.second==2){
+                n.push_back(it.first);
+            }
+        }
+        return n;
+    }
+};
