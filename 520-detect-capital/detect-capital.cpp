ddbe0129b0317f58771e513ff5ccf3b56eeafd03
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-07-01 15:07:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.28%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cnt = 0;
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] >= 'A' && word[i] <= 'Z') {
+                cnt++;
+            }
+        }
+        return (cnt == word.size() || cnt == 0 ||
+                cnt == 1 && (word[0] >= 'A' && word[0] <= 'Z'));
+    }
+};
