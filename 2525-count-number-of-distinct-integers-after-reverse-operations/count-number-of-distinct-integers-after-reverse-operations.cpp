122bf0ce0dd1a6ef27530fa27086a2eb96a88c6d
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-07-03 12:57:59 +0530

    Time: 188 ms (42.27%) | Memory: 110.5 MB (69.19%) - LeetSync

--- 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
+++ 2525-count-number-of-distinct-integers-after-reverse-operations/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int reverse(int n) {
-        if (n < 10)
-            return n;
-        int rev = 0;
-        while (n > 0) {
-            rev = rev * 10 + (n % 10);
-            n = n / 10;
-        }
-        return rev;
-    }
     int countDistinctIntegers(vector<int>& nums) {
-        int p = nums.size();
-        for (int i = 0; i < p; i++) {
-            nums.push_back(reverse(nums[i]));
-        }
-
         unordered_set<int> st(nums.begin(), nums.end());
-        return st.size();
+        for (auto i : nums) {
+            int j = 0;
+            while (i) {
+                j = j * 10 + (i % 10);
+                i /= 10;
+            }
+            st.insert(j);
+        }
+        return (int)st.size();
     }
 };
