4f98ca99044ad9e7009737f56a334686f90e54b6
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-07-04 09:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (20.15%) - LeetSync

--- 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
+++ 3914-check-if-any-element-has-prime-frequency/check-if-any-element-has-prime-frequency.cpp
@@ -3,12 +3,7 @@ public:
     bool prime(int num) {
         if (num <= 1)
             return false;
-        if (num == 2)
-            return true;
-        if (num % 2 == 0)
-            return false;
-
-        for (int i = 3; i * i <= num; i += 2) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0)
                 return false;
         }
