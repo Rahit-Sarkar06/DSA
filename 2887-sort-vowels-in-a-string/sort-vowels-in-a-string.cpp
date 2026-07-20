9c49fd7f79bd00a2653c81110226137406a9ec91
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-07-20 20:17:42 +0530

    Time: 22 ms (24.17%) | Memory: 20.4 MB (5.76%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -6,17 +6,21 @@ public:
     }
     string sortVowels(string s) {
         vector<char> m;
+        vector<int> p;
         for (int i = 0; i < s.size(); i++) {
             if (isVowel(s[i])) {
                 m.push_back(s[i]);
+                p.push_back(i);
             }
         }
         sort(m.begin(), m.end());
         int j = 0;
+        int k = 0;
         for (int i = 0; i < s.size(); i++) {
-            if (isVowel(s[i])) {
+            if (k < p.size() && p[k] == i) {
                 s[i] = m[j];
                 j++;
+                k++;
             }
         }
         return s;
