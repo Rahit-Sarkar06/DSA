df3722f2970ef4c0261a04abb84ff6ebd89d1ce2
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-07-23 13:53:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (93.76%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.cpp
+++ 3567-convert-date-to-binary/convert-date-to-binary.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
     string convertDateToBinary(string date) {
-        string s="";
-        int i=0;
-        int p=0;
-        while(date[i]!='-'){
-            int digit=date[i]-'0';
-            p=(p*10)+digit;
+        string s = "";
+        int i = 0;
+        int p = 0;
+        while (date[i] != '-') {
+            int digit = date[i] - '0';
+            p = (p * 10) + digit;
             i++;
         }
-        while(p>0){
-            s+=(p%2+'0');
-            p/=2;
+        while (p > 0) {
+            s += (p % 2 + '0');
+            p /= 2;
         }
         i++;
-        reverse(s.begin(),s.end());
-        s+="-";
-        p=0;
-        while(date[i]!='-'){
-            int digit=date[i]-'0';
-            p=(p*10)+digit;
+        reverse(s.begin(), s.end());
+        s += "-";
+        p = 0;
+        while (date[i] != '-') {
+            int digit = date[i] - '0';
+            p = (p * 10) + digit;
             i++;
         }
-        string t="";
-        while(p>0){
-            t+=(p%2+'0');
-            p/=2;
+        string t = "";
+        while (p > 0) {
+            t += (p % 2 + '0');
+            p /= 2;
         }
         i++;
-        reverse(t.begin(),t.end());
-        t+="-";
-        p=0;
-        while(i<date.size()){
-            int digit=date[i]-'0';
-            p=(p*10)+digit;
+        reverse(t.begin(), t.end());
+        t += "-";
+        p = 0;
+        while (i < date.size()) {
+            int digit = date[i] - '0';
+            p = (p * 10) + digit;
             i++;
         }
-        string l="";
-        while(p>0){
-            l+=(p%2+'0');
-            p/=2;
+        string l = "";
+        while (p > 0) {
+            l += (p % 2 + '0');
+            p /= 2;
         }
-        reverse(l.begin(),l.end());
+        reverse(l.begin(), l.end());
 
-        return s+t+l;
-
+        return s + t + l;
     }
 };
