9b21453384115847b99c680a3cdd1facb2ce0860
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-08-18 11:29:58 +0530

    Time: 3 ms (72.55%) | Memory: 27.6 MB (32.80%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* makeTree(vector<int>& preorder, int preStart, int preEnd,
+                       vector<int>& inorder, int inStart, int inEnd,
+                       map<int, int>& inmap) {
+        if (preStart > preEnd || inStart > inEnd)
+            return NULL;
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int inRoot = inmap[root->val];
+        int numsLeft = inRoot - inStart;
+        root->left = makeTree(preorder, preStart + 1, preStart + numsLeft,
+                              inorder, inStart, inRoot - 1, inmap);
+        root->right = makeTree(preorder, preStart + 1 + numsLeft, preEnd,
+                               inorder, inRoot + 1, inEnd, inmap);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int> imap;
+        for (int i = 0; i < inorder.size(); i++) {
+            imap[inorder[i]] = i;
+        }
+        TreeNode* root = makeTree(preorder, 0, preorder.size() - 1, inorder, 0,
+                                  inorder.size() - 1, imap);
+        return root;
+    }
+};
