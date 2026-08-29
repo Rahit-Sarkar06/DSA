766772fb7b4841edea3c48de9c919576d29a4da0
Rahit-Sarkar06 <sarkarrahit2024@gmail.com>
2026-08-29 11:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (76.60%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -1,10 +1,4 @@
 class Solution {
 public:
-    int minOperations(int n) {
-        int cnt = 0;
-        for (int i = 1; i <= n; i += 2) {
-            cnt += (n - i);
-        }
-        return cnt;
-    }
+    int minOperations(int n) { return (n * n) / 4; }
 };
